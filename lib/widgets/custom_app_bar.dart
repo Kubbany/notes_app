5a486655613be363f17224c7bb7b1b0e52162a21
Kubbany <2021170289@cis.asu.edu.eg>
2024-08-18 15:05:57 +0300

    create custom app bar

---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text(
+          "Notes",
+          style: TextStyle(
+            fontSize: 28,
+          ),
+        ),
+        FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.grey.withOpacity(0.05),
+          elevation: 0,
+          child: const Icon(
+            Icons.search,
+            size: 30,
+          ),
+        ),
+      ],
+    );
+  }
+}
