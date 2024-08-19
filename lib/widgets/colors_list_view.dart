988399e43ba241950581982789ad0e67b8f12fa5
Kubbany <2021170289@cis.asu.edu.eg>
2024-08-20 00:30:03 +0300

    colors list view added

---
+++ lib/widgets/colors_list_view.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ColorItem extends StatelessWidget {
+  const ColorItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CircleAvatar(
+      radius: 38,
+      backgroundColor: Colors.blue,
+    );
+  }
+}
+
+class ColorsListView extends StatelessWidget {
+  const ColorsListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 38 * 2,
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return const ColorItem();
+        },
+        separatorBuilder: (context, index) {
+          return const SizedBox(
+            width: 10,
+          );
+        },
+        itemCount: 10,
+      ),
+    );
+  }
+}
