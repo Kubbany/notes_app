36d45f9e81074082ee883523fddfcc1b637dbad2
Kubbany <2021170289@cis.asu.edu.eg>
2024-08-19 13:48:01 +0300

    fetch all notes method created

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+  fetchAllNotes() async {
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+
+      List<NoteModel> notes = notesBox.values.toList();
+      emit(NotesSuccess(notes: notes));
+    } catch (e) {
+      emit(NotesFailure(errorMessage: e.toString()));
+    }
+  }
 }
