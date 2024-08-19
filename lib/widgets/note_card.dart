e4108e8b3ebc2cfd1cd155bc2c03a4096ae32834
Kubbany <2021170289@cis.asu.edu.eg>
2024-08-19 22:13:40 +0300

    delete note added

--- lib/widgets/note_card.dart
+++ lib/widgets/note_card.dart
@@ -45,7 +45,9 @@ class NoteCard extends StatelessWidget {
                 ),
               ),
               trailing: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  note.delete();
+                },
                 icon: const Icon(
                   Icons.delete,
                   color: Colors.black,
